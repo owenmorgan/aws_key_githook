d03e0fc2725dc7f67f86fa5e121a0a31830fcbcb
Owen Morgan <owen.morgan@oneiota.co.uk>
2017-01-26 15:09:01 +0000

    setup script.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-git config --global init.templatedir '~/.git-templates'
-mkdir -p ~/.git-templates/hooks
-curl -o ~/.git-templates/hooks/pre-commit "https://raw.githubusercontent.com/bnathyuw/aws-key-git-hook/master/pre-commit"
-chmod a+x ~/.git-templates/hooks/pre-commit
+# git config --global init.templatedir '~/.git-templates'
+# mkdir -p ~/.git-templates/hooks
+# curl -o ~/.git-templates/hooks/pre-commit "https://raw.githubusercontent.com/bnathyuw/aws-key-git-hook/master/pre-commit"
+# chmod a+x ~/.git-templates/hooks/pre-commit
+
+mkdir -p  ~/.git/hooks/
+curl -o ~/.git/hooks/pre-commit-aws "https://raw.githubusercontent.com/bnathyuw/aws-key-git-hook/master/pre-commit"
+chmod a+x ~/.git/hooks/pre-commit-aws
