3dd1d5f02456ca70ad2a15be5740169183cb1155
Owen Morgan <owen.morgan@oneiota.co.uk>
2017-01-26 15:45:04 +0000

    setup script.

--- setup.sh
+++ setup.sh
@@ -3,3 +3,10 @@
 mkdir -p  ~/.git/hooks/pre-commit
 curl -o ~/.git/hooks/pre-commit/pre-commit-aws "https://raw.githubusercontent.com/bnathyuw/aws-key-git-hook/master/pre-commit"
 chmod a+x ~/.git/hooks/pre-commit/pre-commit-aws
+
+streeappdir="/Applications/SourceTree.app/Contents/Resources/git_local/share/git-core/templates/hooks"
+if [ -d "$streeappdir" ]; then
+    echo "Installing for SourceTree"
+    curl -o $streeappdir/pre-commit-aws "https://raw.githubusercontent.com/bnathyuw/aws-key-git-hook/master/pre-commit"
+    chmod a+x $streeappdir/pre-commit-aws
+fi
