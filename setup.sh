6234adec040b7a9b5152a683dfb05a8605085f4b
Owen Morgan <owen.morgan@oneiota.co.uk>
2017-01-26 16:18:57 +0000

    renamed. and removed source tree.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
 mkdir -p  ~/.git-templates/hooks
-curl -o ~/.git-templates/hooks/pre-commit-aws "https://raw.githubusercontent.com/bnathyuw/aws-key-git-hook/master/pre-commit"
+curl -o ~/.git-templates/hooks/pre-commit-aws "https://raw.githubusercontent.com/owenmorgan/aws_key_githook/master/pre-commit-aws"
 chmod a+x ~/.git-templates/hooks/pre-commit-aws
 
 git config --global core.hooksPath '~/.git-templates/hooks'
 git config --global init.templatedir '~/.git-templates'
 
-streeappdir="/Applications/SourceTree.app/Contents/Resources/git_local/share/git-core/templates/hooks"
-if [ -d "$streeappdir" ]; then
-    echo "Installing for SourceTree"
-    curl -o $streeappdir/pre-commit-aws "https://raw.githubusercontent.com/bnathyuw/aws-key-git-hook/master/pre-commit"
-    chmod a+x $streeappdir/pre-commit-aws
-fi
