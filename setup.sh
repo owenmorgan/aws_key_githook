21eb27995b53b207c41fdc3259b04208761e9d3a
Owen Morgan <owen.morgan@oneiota.co.uk>
2017-01-26 15:49:55 +0000

    setup script.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-mkdir -p  ~/.git/hooks/pre-commit
-curl -o ~/.git/hooks/pre-commit/pre-commit-aws "https://raw.githubusercontent.com/bnathyuw/aws-key-git-hook/master/pre-commit"
-chmod a+x ~/.git/hooks/pre-commit/pre-commit-aws
+mkdir -p  ~/.git-templates/hooks
+curl -o ~/.git-templates/hooks/pre-commit-aws "https://raw.githubusercontent.com/bnathyuw/aws-key-git-hook/master/pre-commit"
+chmod a+x ~/.git-templates/hooks/pre-commit-aws
+
+git config --global core.hooksPath '~/.git-templates/hooks'
+git config --global init.templatedir '~/.git-templates'
 
 streeappdir="/Applications/SourceTree.app/Contents/Resources/git_local/share/git-core/templates/hooks"
 if [ -d "$streeappdir" ]; then
